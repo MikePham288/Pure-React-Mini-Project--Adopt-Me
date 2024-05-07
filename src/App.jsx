801d23dafddb844f437a7c3462c70757538f560a
Mike Pham <mikep@xello.world>
2024-05-07 06:45:55 -0400

    update eslint with React support

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Milo" animal="dog" breed="Husky"></Pet>
-    <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
-    <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet>
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Milo" animal="dog" breed="Husky"></Pet>
+      <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
+      <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
