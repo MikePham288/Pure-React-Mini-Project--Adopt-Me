3f1a90073c6eec87ce9ac212876abdd6ebf138c7
Mike Pham <mikep@xello.world>
2024-05-07 06:34:58 -0400

    Move Pet into its own jsx component & Re-write code for jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, // handle attributes here
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Milo", animal: "dog", breed: "Husky" }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "Mixed",
-      }),
-    ]
-  );
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Milo" animal="dog" breed="Husky"></Pet>
+    <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
+    <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet>
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container); // concurrency, used to be static mode & concurrent mode.
-root.render(React.createElement(App));
+root.render(<App />);
 // tree shaking - live code inclusion
