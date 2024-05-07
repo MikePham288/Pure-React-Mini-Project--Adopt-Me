fbb6e83c10ab64baee2b7adede6f8baf1315c8e7
Mike Pham <mikep@xello.world>
2024-05-07 04:37:23 -0400

    move js code into App.js & learning about React one-way data flow

---
+++ src/App.js
@@ -0,0 +1,25 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {}, // handle attributes here
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {name: 'Milo', animal: 'dog', breed: 'Husky'}),
+            React.createElement(Pet, {name: 'Pepper', animal: 'bird', breed: 'Cockatiel'}),
+            React.createElement(Pet, {name: 'Doink', animal: 'cat', breed: 'Mixed'}),
+        ]
+    )
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container); // concurrency, used to be static mode & concurrent mode.
+root.render(React.createElement(App));
