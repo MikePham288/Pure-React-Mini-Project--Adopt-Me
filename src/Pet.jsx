a474243beeb65fbc745cf72a5ff4f87c7ba78e49
Mike Pham <mikep@xello.world>
2024-05-12 01:50:58 -0400

    refactor: styling pets to look better

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -5,14 +5,33 @@
 //     React.createElement("h2", {}, props.breed),
 //   ]);
 // };
-const Pet = (props) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.animal}</h1>
-      <h1>{props.breed}</h1>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
   );
+
+  // return (
+  //   <div>
+  //     <h1>{props.name}</h1>
+  //     <h2>{props.animal}</h2>
+  //     <h2>{props.breed}</h2>
+  //   </div>
+  // );
 };
 
 export default Pet;
