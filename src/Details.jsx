7605bfabdd0ef74c65b6e97b73426b620f868cf3
Mike Pham <mikep@xello.world>
2024-05-12 11:13:44 -0400

    performance optimization with React Query in details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>hi, {id}!</h2>;
+  const result = useQuery(["details", id], fetchPet);
+
+  if (result.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = result.data.pets[0];
+  return (
+    // <h2>hi, {id}!</h2>
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+          <button> Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
