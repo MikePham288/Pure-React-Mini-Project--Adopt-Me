95a73b2ee131880573d05199af8deec452574091
Mike Pham <mikep@xello.world>
2024-05-12 11:02:50 -0400

    feat: Setup React Query on App

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,36 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me</Link>
-      </header>
-      {/* <div> */}
-      {/* <h1>Adopt Me!</h1> */}
-      <Routes>
-        <Route path="/details/:id" element={<Details />}></Route>
-        <Route path="/" element={<SearchParams />}></Route>
-      </Routes>
-      {/* <SearchParams></SearchParams> */}
-      {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        {/* <div> */}
+        {/* <h1>Adopt Me!</h1> */}
+        <Routes>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+        {/* <SearchParams></SearchParams> */}
+        {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
       <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
       <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet> */}
-      {/* </div> */}
+        {/* </div> */}
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
