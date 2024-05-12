89ae39319b426e7925065a252eb778492033607a
Mike Pham <mikep@xello.world>
2024-05-12 14:15:26 -0400

    feat: Create global context for the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import SearchParams from "./SearchParams";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Details from "./Details";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,23 +16,26 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me</Link>
-        </header>
-        {/* <div> */}
-        {/* <h1>Adopt Me!</h1> */}
-        <Routes>
-          <Route path="/details/:id" element={<Details />}></Route>
-          <Route path="/" element={<SearchParams />}></Route>
-        </Routes>
-        {/* <SearchParams></SearchParams> */}
-        {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me</Link>
+          </header>
+          {/* <div> */}
+          {/* <h1>Adopt Me!</h1> */}
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+          {/* <SearchParams></SearchParams> */}
+          {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
       <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
       <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet> */}
-        {/* </div> */}
+          {/* </div> */}
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
