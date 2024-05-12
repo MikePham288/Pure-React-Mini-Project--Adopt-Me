7c88a16deed06359a47e62f923e6f3c57adaedd2
Mike Pham <mikep@xello.world>
2024-05-12 01:26:48 -0400

    feat: learn how to use effect to call API to set options for animals & breeds & use native fetch functionality

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
+const ANIMALS = ["dog", "cat", "bird", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Toronto, ON");
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreeds] = useState("");
+  const [pets, setPets] = useState([]);
   // Equivalent
   // const locationHook - useState('');
   // const location = locationHook[0];
   // const setLocation = locationHook[1];
 
+  useEffect(() => {
+    // run the same as ComponentDidMount
+    requestPets(); // runs every times there's an event without an empty array. Empty array makes it run only on the first render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+    setPets(json.pets);
+  }
+
   return (
     <div className="search-params">
-      <form action="">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -20,8 +45,55 @@ const SearchParams = () => {
           />
         </label>
 
+        <label htmlFor="animal">
+          Animal
+          <select
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreeds("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreeds("");
+            }}
+            id="animal"
+          >
+            <option value="" />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            value={breed}
+            onChange={(e) => {
+              setBreeds(e.target.value);
+            }}
+            id="breed"
+            disabled={breed.length === 0}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
+
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
