fcb5ce9062b4fb577aeb09bddca19728cb3db4e3
Mike Pham <mikep@xello.world>
2024-05-20 15:27:54 -0400

    feat:   Intermediate React V5 - setup Server Side Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense, lazy, useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
@@ -26,45 +25,42 @@ const App = () => {
           "url('https://pets-images.dev-apis.com/pets/wallpaperA.jpg')",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPet}>
-            <Suspense
-              fallback={
-                <div className="flex items-center justify-center p-4">
-                  <h2 className="animate-spin text-[100px]">🌀</h2>
-                </div>
-              }
-            >
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me
-                </Link>
-              </header>
-              {/* <div> */}
-              {/* <h1>Adopt Me!</h1> */}
-              <Routes>
-                <Route path="/details/:id" element={<Details />}></Route>
-                <Route path="/" element={<SearchParams />}></Route>
-              </Routes>
-            </Suspense>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center p-4">
+                <h2 className="animate-spin text-[100px]">🌀</h2>
+              </div>
+            }
+          >
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me
+              </Link>
+            </header>
+            {/* <div> */}
+            {/* <h1>Adopt Me!</h1> */}
+            <Routes>
+              <Route path="/details/:id" element={<Details />}></Route>
+              <Route path="/" element={<SearchParams />}></Route>
+            </Routes>
+          </Suspense>
 
-            {/* <SearchParams></SearchParams> */}
-            {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
+          {/* <SearchParams></SearchParams> */}
+          {/* <Pet name="Milo" animal="dog" breed="Husky"></Pet>
       <Pet name="Pepper" animal="dog" breed="Husky"></Pet>
       <Pet name="Doink" animal="cat" breed="CockMixedtiel"></Pet> */}
-            {/* </div> */}
-          </AdoptedPetContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+          {/* </div> */}
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container); // concurrency, used to be static mode & concurrent mode.
-root.render(<App />);
+// const container = document.getElementById("root"); // SSR
+// const root = createRoot(container); // concurrency, used to be static mode & concurrent mode.
+// root.render(<App />);
 // tree shaking - live code inclusion
+
+export default App;
---
+++ src/ClientApp.jsx
@@ -0,0 +1,10 @@
+const { hydrateRoot } = require("react-dom/client");
+const { BrowserRouter } = require("react-router-dom");
+const { default: App } = require("./App");
+
+hydrateRoot(
+  document.getElementById("root"),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
